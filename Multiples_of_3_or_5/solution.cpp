62afb9d5e194a1adc9e9b0b544d2a3f0163ba055
Ivan Ateyea <iateyea@gmail.com>
2021-11-29 00:03:51 -0600

    Tidied up the solution + added problem statement

--- Multiples_of_3_or_5/solution.cpp
+++ Multiples_of_3_or_5/solution.cpp
@@ -1,17 +1,13 @@
+// Find the sum of all the multiples of 3 or 5 below 1000.
 #include <iostream>
 
 int main(){
 
     int sum = 0;
     for(int i = 1; i < 1000; i++){
-        if (i % 3 == 0 && i % 5 == 0){
+        if (i % 3 == 0 || i % 5 == 0){
             sum += i;
-        }
-        else if (i % 3 == 0){
-            sum += i;
-        }else if (i % 5 == 0){
-            sum += i;
-        }
+            }
     }
 
     return sum;
